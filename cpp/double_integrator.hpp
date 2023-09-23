83b63adec308e4313a6817fe209d3bca8f58d3ce
HiroIshida <spitfire.docomo@gmail.com>
2023-09-23 16:08:49 +0900

    refactor: split source and header

--- cpp/double_integrator.hpp
+++ cpp/double_integrator.hpp
@@ -10,114 +10,13 @@ namespace double_integrator_planning {
 
 double bisection_newton(const std::function<double(double)> &f,
                         const std::function<double(double)> &df, double start,
-                        double end, double tol = 0.05, int max_iter = 20) {
-  double est = (start + end) * 0.5;
-  for (int i = 0; i < max_iter; ++i) {
-    double fratio = f(est) / df(est);
+                        double end, double tol = 0.05, int max_iter = 20);
 
-    if ((end - (est - fratio)) * ((est - fratio) - start) < 0.0 ||
-        std::abs(fratio) < (end - start) / 4.0) {
-      if (f(est) > 0) {
-        end = est;
-      } else {
-        start = est;
-      }
-      fratio = est - (end + start) * 0.5;
-    }
-    est -= fratio;
+std::pair<double, double> optimal_cost(const State &s0, const State &s1);
 
-    if (std::abs(fratio) < tol) {
-      break;
-    }
-  }
-  return est;
-}
+BoundingBox forward_reachable_box(const State &state, double r);
 
-std::pair<double, double> optimal_cost(const State &s0, const State &s1) {
-  const Eigen::Vector2d &x0 = s0.x;
-  const Eigen::Vector2d &v0 = s0.v;
-  const Eigen::Vector2d &x1 = s1.x;
-  const Eigen::Vector2d &v1 = s1.v;
-  Eigen::Vector2d x_diff = x1 - x0;
-  Eigen::Vector2d v_diff = v1 - v0;
-
-  double p = -4 * (v0.dot(v0) + v1.dot(v1) + v0.dot(v1));
-  double q = 24 * (v0 + v1).dot(x_diff);
-  double r = -36 * x_diff.dot(x_diff);
-
-  auto cost = [&](double t) {
-    return t + v_diff.dot(4.0 * v_diff / t - 6 * (-v0 * t + x_diff) / (t * t)) +
-           (-6 * v_diff / (t * t) + 12 * (x_diff - v0 * t) / (t * t * t))
-               .dot(-v0 * t + x_diff);
-  };
-
-  auto cost_d1 = [&](double t) {
-    return t * t * t * t + p * t * t + q * t + r;
-  };
-
-  auto cost_d2 = [&](double t) { return 4.0 * t * t * t + 2 * p * t + q; };
-
-  double t_min = 0.0;
-  double t_max = 10.0;
-  double optimal_t = bisection_newton(cost_d1, cost_d2, t_min, t_max);
-  return {cost(optimal_t), optimal_t};
-}
-
-BoundingBox forward_reachable_box(const State &state, double r) {
-  Eigen::Vector2d tau_x_plus, tau_x_minus, xmax, xmin, vmax, vmin;
-  const Eigen::Vector2d &x0 = state.x;
-  const Eigen::Vector2d &v0 = state.v;
-
-  tau_x_plus = 2.0 / 3.0 *
-               (-v0.array().square() + r +
-                (v0.array() * (v0.array().square() + r).sqrt()));
-  tau_x_minus = 2.0 / 3.0 *
-                (-v0.array().square() + r -
-                 (v0.array() * (v0.array().square() + r).sqrt()));
-
-  xmax = (v0.cwiseProduct(tau_x_plus) + x0).array() +
-         ((1.0 / 3.0) * tau_x_plus.array().square() * (-tau_x_plus.array() + r))
-             .sqrt();
-  xmin =
-      (v0.cwiseProduct(tau_x_minus) + x0).array() -
-      ((1.0 / 3.0) * tau_x_minus.array().square() * (-tau_x_minus.array() + r))
-          .sqrt();
-
-  double tau_v_plus = 0.5 * r;
-  vmax =
-      v0 + Eigen::Vector2d::Constant(std::sqrt(tau_v_plus * (-tau_v_plus + r)));
-  vmin =
-      v0 - Eigen::Vector2d::Constant(std::sqrt(tau_v_plus * (-tau_v_plus + r)));
-  return BoundingBox{xmin, xmax, vmin, vmax};
-}
-
-BoundingBox backward_reachable_box(const State &state, double r) {
-  Eigen::Vector2d tau_x_plus, tau_x_minus, xmax, xmin, vmax, vmin;
-  const Eigen::Vector2d &x0 = state.x;
-  const Eigen::Vector2d &v0 = state.v;
-
-  tau_x_plus = 2.0 / 3.0 *
-               (v0.array().square() - r +
-                (v0.array() * (v0.array().square() + r).sqrt()));
-  tau_x_minus = 2.0 / 3.0 *
-                (v0.array().square() - r -
-                 (v0.array() * (v0.array().square() + r).sqrt()));
-
-  xmax = (v0.cwiseProduct(tau_x_plus) + x0).array() +
-         ((1.0 / 3.0) * tau_x_plus.array().square() * (tau_x_plus.array() + r))
-             .sqrt();
-  xmin =
-      (v0.cwiseProduct(tau_x_minus) + x0).array() -
-      ((1.0 / 3.0) * tau_x_minus.array().square() * (tau_x_minus.array() + r))
-          .sqrt();
-
-  double tau_v_plus = 0.5 * r;
-  vmax =
-      v0 + Eigen::Vector2d::Constant(std::sqrt(tau_v_plus * (tau_v_plus + r)));
-  vmin =
-      v0 - Eigen::Vector2d::Constant(std::sqrt(tau_v_plus * (tau_v_plus + r)));
-  return BoundingBox{xmin, xmax, vmin, vmax};
-}
+BoundingBox backward_reachable_box(const State &state, double r);
 
 class TrajectoryPiece {
 public:
@@ -128,43 +27,8 @@ private:
   Eigen::Vector4d _s1;
 
 public:
-  TrajectoryPiece(const State &s0, const State &s1, double duration)
-      : duration_(duration) {
-    Eigen::Vector2d x0 = s0.x;
-    Eigen::Vector2d v0 = s0.v;
-    Eigen::Vector2d x1 = s1.x;
-    Eigen::Vector2d v1 = s1.v;
-
-    Eigen::Vector2d x01 = x1 - x0;
-    Eigen::Vector2d v01 = v1 - v0;
-
-    _d.head<2>() =
-        -6.0 * v01 / (duration * duration) +
-        12.0 * (-duration * v0 + x01) / (duration * duration * duration);
-    _d.tail<2>() = 4.0 * v01 / duration -
-                   6.0 * (-duration * v0 + x01) / (duration * duration);
-    // concat x and v into a single vector
-    _s1 = s1.to_vector();
-  }
-
-  State interpolate(double t) const {
-    double s = t - duration_;
-    Eigen::Matrix4d M_left = Eigen::Matrix4d::Zero();
-    Eigen::Matrix4d M_right = Eigen::Matrix4d::Zero();
-
-    M_left.block<2, 2>(0, 0) = Eigen::Matrix2d::Identity();
-    M_left.block<2, 2>(0, 2) = Eigen::Matrix2d::Identity() * s;
-    M_left.block<2, 2>(2, 2) = Eigen::Matrix2d::Identity();
-
-    M_right.block<2, 2>(0, 0) =
-        Eigen::Matrix2d::Identity() * (-s * s * s / 6.0);
-    M_right.block<2, 2>(0, 2) = Eigen::Matrix2d::Identity() * s * s * 0.5;
-    M_right.block<2, 2>(2, 0) = Eigen::Matrix2d::Identity() * (-s * s * 0.5);
-    M_right.block<2, 2>(2, 2) = Eigen::Matrix2d::Identity() * s;
-
-    auto vec = M_left * _s1 + M_right * _d;
-    return State{vec.head<2>(), vec.tail<2>()};
-  }
+  TrajectoryPiece(const State &s0, const State &s1, double duration);
+  State interpolate(double t) const;
 };
 
 } // namespace double_integrator_planning
--- cpp/rrt.hpp
+++ cpp/rrt.hpp
@@ -22,106 +22,13 @@ public:
   RRT(const State &start, const State &goal,
       std::function<bool(State)> is_obstacle_free,
       const BoundingBox &state_bound, const double dt_extend,
-      const double dt_resolution)
-      : start_(start), goal_(goal), is_obstacle_free_(is_obstacle_free),
-        state_bound_(state_bound), dt_extend_(dt_extend),
-        dt_resolution_(dt_resolution) {
-    this->nodes_.push_back(
-        std::make_shared<Node>(Node{start, nullptr, {}, {}}));
-  }
+      const double dt_resolution);
+  bool is_valid(const State &state) const;
+  std::shared_ptr<Node> find_nearest(const State &state) const;
 
-  bool is_valid(const State &state) const {
-    return is_obstacle_free_(state) and state_bound_.is_inside(state);
-  }
-
-  std::shared_ptr<Node> find_nearest(const State &state) const {
-    std::shared_ptr<Node> nearest_node = nullptr;
-    double min_dist = std::numeric_limits<double>::max();
-    for (const auto &node : this->nodes_) {
-      const double dist = (node->state.to_vector() - state.to_vector()).norm();
-      if (dist < min_dist) {
-        min_dist = dist;
-        nearest_node = node;
-      }
-    }
-    return nearest_node;
-  }
-
-  ExtendResult extend(const State &s_target) {
-    if (!is_valid(s_target)) {
-      return ExtendResult::TRAPPED;
-    }
-
-    const auto nearest_node = find_nearest(s_target);
-    auto [_, t_optimal] = optimal_cost(nearest_node->state, s_target);
-    auto traj_piece = TrajectoryPiece(nearest_node->state, s_target, t_optimal);
-    double t = dt_resolution_;
-    std::optional<State> state_to_add = std::nullopt;
-    ExtendResult er = ExtendResult::TRAPPED;
-    while (t < dt_extend_) {
-      auto state = traj_piece.interpolate(t);
-      if (not is_valid(state)) {
-        if (state_to_add.has_value()) {
-          this->nodes_.push_back(
-              std::make_shared<Node>(Node{*state_to_add, nearest_node, t, {}}));
-        }
-        return er;
-      } else {
-        state_to_add = std::move(state);
-        er = ExtendResult::ADVANCED;
-      }
-      t += dt_resolution_;
-    }
-    if (state_to_add.has_value()) {
-      this->nodes_.push_back(std::make_shared<Node>(
-          Node{*state_to_add, nearest_node, dt_extend_, {}}));
-    }
-    return ExtendResult::REACHED;
-  }
-
-  bool solve(size_t max_iter) {
-    for (size_t i = 0; i < max_iter; ++i) {
-      const auto s_rand = state_bound_.sample();
-      const auto extend_result = extend(s_rand);
-      if (extend_result != ExtendResult::TRAPPED) {
-        auto [time_optimal, _] = optimal_cost(nodes_.back()->state, goal_);
-        if (time_optimal < dt_extend_) {
-          auto traj_piece =
-              TrajectoryPiece(nodes_.back()->state, goal_, time_optimal);
-
-          auto is_connectable = [&]() {
-            double t = dt_resolution_;
-            while (t < time_optimal) {
-              auto state = traj_piece.interpolate(t);
-              if (not is_valid(state)) {
-                return false;
-              }
-              t += dt_resolution_;
-            }
-            return true;
-          };
-
-          if (is_connectable()) {
-            nodes_.push_back(std::make_shared<Node>(
-                Node{goal_, nodes_.back(), time_optimal}));
-            return true;
-          }
-        }
-      }
-    }
-    return false;
-  }
-
-  std::vector<std::shared_ptr<Node>> get_solution() const {
-    std::vector<std::shared_ptr<Node>> solution;
-    auto node = nodes_.back();
-    while (node != nullptr) {
-      solution.push_back(node);
-      node = node->parent;
-    }
-    std::reverse(solution.begin(), solution.end());
-    return solution;
-  }
+  ExtendResult extend(const State &s_target);
+  bool solve(size_t max_iter);
+  std::vector<std::shared_ptr<Node>> get_solution() const;
 
 public:
   State start_;
@@ -149,97 +56,13 @@ public:
   FastMarchingTree(const State &start, const State &goal,
                    std::function<bool(State)> is_obstacle_free,
                    const BoundingBox &state_bound, double dt_resolution,
-                   double admissible_cost, size_t N)
-      : start_(start), goal_(goal), is_obstacle_free_(is_obstacle_free),
-        state_bound_(state_bound), dt_(dt_resolution),
-        addmissible_cost_(admissible_cost) {
-    nodes_.push_back(std::make_shared<NodeWithStatus>(
-        NodeWithStatus{start_, nullptr, {}, 0.0, FMTNodeStatus::OPEN}));
-    // sample N state
-    for (size_t i = 0; i < N; ++i) {
-      while (true) {
-        auto s_rand = state_bound_.sample();
-        if (is_valid(s_rand)) {
-          nodes_.push_back(std::make_shared<NodeWithStatus>(NodeWithStatus{
-              s_rand, nullptr, {}, {}, FMTNodeStatus::UNVISITED}));
-          break;
-        }
-      }
-    }
-    nodes_.push_back(std::make_shared<NodeWithStatus>(
-        NodeWithStatus{goal_, nullptr, {}, {}, FMTNodeStatus::UNVISITED}));
-  }
+                   double admissible_cost, size_t N);
 
-  bool is_solved() {
-    auto &goal_node = nodes_.back();
-    return goal_node->status != FMTNodeStatus::UNVISITED;
-  }
-  bool is_failed() { return get_num_status(FMTNodeStatus::OPEN) == 0; }
+  bool is_solved();
+  bool is_failed();
+  size_t get_num_status(FMTNodeStatus status);
 
-  size_t get_num_status(FMTNodeStatus status) {
-    size_t num = 0;
-    for (const auto &node : nodes_) {
-      if (node->status == status) {
-        num++;
-      }
-    }
-    return num;
-  }
-
-  void extend() {
-    std::shared_ptr<NodeWithStatus> open_node_best = nullptr;
-    double open_node_cost_min = std::numeric_limits<double>::max();
-    for (const auto &node : nodes_) {
-      if (node->status == FMTNodeStatus::OPEN) {
-        if (*node->cost < open_node_cost_min) {
-          open_node_best = node;
-          open_node_cost_min = *node->cost;
-        }
-      }
-    }
-    assert(open_node_best->status == FMTNodeStatus::OPEN);
-
-    auto [nodes_reachable, _, __] =
-        filter_reachable(open_node_best->state, addmissible_cost_,
-                         FilterMode::FORWARD, FMTNodeStatus::UNVISITED, false);
-    for (const auto &node : nodes_reachable) {
-      assert(node->status == FMTNodeStatus::UNVISITED);
-      auto [nodes_cand, costs_cand, times_cand] =
-          filter_reachable(node->state, addmissible_cost_, FilterMode::BACKWARD,
-                           FMTNodeStatus::OPEN, true);
-      if (nodes_cand.empty())
-        continue;
-
-      double cost_optimal = std::numeric_limits<double>::max();
-      size_t i_optimal = 0;
-      for (size_t i = 0; i < nodes_cand.size(); ++i) {
-        double cost = costs_cand[i] + *nodes_cand[i]->cost;
-        if (cost < cost_optimal) {
-          cost_optimal = cost;
-          i_optimal = i;
-        }
-      }
-      std::shared_ptr<NodeWithStatus> parent_optimal = nodes_cand[i_optimal];
-      double time_optimal = times_cand[i_optimal];
-      auto is_connectable = [&]() {
-        auto traj =
-            TrajectoryPiece(parent_optimal->state, node->state, time_optimal);
-        for (double t = dt_; t < time_optimal; t += dt_) {
-          if (!is_valid(traj.interpolate(t))) {
-            return false;
-          }
-        }
-        return true;
-      };
-      if (is_connectable()) {
-        node->parent = parent_optimal;
-        node->cost = *parent_optimal->cost + cost_optimal;
-        node->status = FMTNodeStatus::OPEN;
-        node->duration_from_parent = time_optimal;
-      }
-    }
-    open_node_best->status = FMTNodeStatus::CLOSED;
-  }
+  void extend();
 
   enum class FilterMode { FORWARD, BACKWARD };
 
@@ -247,48 +70,9 @@ public:
              std::vector<double>>
   filter_reachable(const State &s_center, double admisible_cost,
                    FilterMode mode, FMTNodeStatus status,
-                   bool return_costs) const {
-    std::vector<std::shared_ptr<NodeWithStatus>> node_reachable;
-    std::vector<double> costs_reachable;
-    std::vector<double> times_reachable;
-
-    auto box = (mode == FilterMode::FORWARD)
-                   ? forward_reachable_box(s_center, admisible_cost)
-                   : backward_reachable_box(s_center, admisible_cost);
-    for (const auto &node : nodes_) {
-      if (node->status != status)
-        continue;
-      if (box.is_inside(node->state)) {
-        auto [c, t] = (mode == FilterMode::FORWARD)
-                          ? optimal_cost(s_center, node->state)
-                          : optimal_cost(node->state, s_center);
-        if (c < admisible_cost) {
-          node_reachable.push_back(node);
-          if (return_costs) {
-            costs_reachable.push_back(c);
-            times_reachable.push_back(t);
-          }
-        }
-      }
-    }
-    return {node_reachable, costs_reachable, times_reachable};
-  }
-
-  bool is_valid(const State &state) const {
-    return is_obstacle_free_(state) and state_bound_.is_inside(state);
-  }
-
-  std::vector<TrajectoryPiece> get_solution() const {
-    std::vector<TrajectoryPiece> solution;
-    auto node = nodes_.back();
-    while (node->parent != nullptr) {
-      solution.push_back(TrajectoryPiece(node->parent->state, node->state,
-                                         *node->duration_from_parent));
-      node = node->parent;
-    }
-    std::reverse(solution.begin(), solution.end());
-    return solution;
-  }
+                   bool return_costs) const;
+  bool is_valid(const State &state) const;
+  std::vector<TrajectoryPiece> get_solution() const;
 
 public:
   State start_;
